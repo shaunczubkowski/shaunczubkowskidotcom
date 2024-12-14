a92904b3d84bd606000a73e1c56144f60143d57b
Shaun Czubkowski <shaunczubkowski@outlook.com>
2024-12-14 15:14:01 -0700

    Add additional details to about page.

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,42 +1,56 @@
 import AnimatedSection from "./layout/AnimatedSection";
 
 const About = () => {
-    return (
-        <section className="xl:pt-40 pt-36 w-full" id="about">
-            <AnimatedSection>
-                <h3 className="text-4xl my-4 font-bold text-accent text-center">About</h3>
-                <div className="flex flex-col  xl:flex-row text-text">
-                    <img className="w-[300px] h-[300px] xl:w-[500px] xl:h-[500px] self-center" src="/shaun.png" alt="Shaun Czubkowski" />
-                    <article className="self-center mx-7 text-lg">
-                        <p className="my-4">
-                            Hi, I'm Shaun Czubkowski, a full-stack software engineer based in Salt Lake City, UT.
-                            I have a passion for creating smooth, intuitive user experiences, and while I naturally
-                            gravitate toward front-end development, I'm always up for tackling challenges across the
-                            entire software stack—from DevOps to backend development.
-                        </p>
-                        <p className="my-4">
-                            What really drives me is the opportunity to solve problems for users. I believe that understanding
-                            the people behind the problems is key to building solutions that not only meet their needs but also
-                            make their lives easier. Whether I'm collaborating with product owners, designers, or users, I focus
-                            on empathy and communication to create products that make a real impact.
-                        </p>
-                        <p className="my-4">
-                            Over the years, I've had the privilege of working on a diverse range of projects—from building a mobile
-                            app to help users locate items, to building a data quality interface, and even streamlining internationalization
-                            processes. I've worked in industries like sustainability and real estate, helping organizations measure and manage
-                            their environmental impacts, as well as building internal tools to support retail operations at a growing midwestern
-                            company.
-                        </p>
-                        <p className="my-4">
-                            No matter the project, my goal is to bring a mix of technical expertise, creativity, and empathy to the table. I’m
-                            always excited to dive into new challenges and collaborate with others to create something meaningful.
-                        </p>
-                    </article>
-
-                </div>
-            </AnimatedSection>
-        </section >
-    );
-}
+  return (
+    <section className="xl:pt-40 pt-36 w-full" id="about">
+      <AnimatedSection>
+        <h3 className="text-4xl my-4 font-bold text-accent text-center">
+          About
+        </h3>
+        <div className="flex flex-col  xl:flex-row text-text">
+          <img
+            className="w-[300px] h-[300px] xl:w-[500px] xl:h-[500px] self-center"
+            src="/shaun.png"
+            alt="Shaun Czubkowski"
+          />
+          <article className="self-center mx-7 text-lg">
+            <p className="my-4">
+              Hi, I'm Shaun Czubkowski, a full-stack software engineer based in
+              Salt Lake City, UT. I have a passion for creating smooth,
+              intuitive user experiences, and while I naturally gravitate toward
+              front-end development, I'm always up for tackling challenges
+              across the entire software stack—from DevOps to backend
+              development.
+            </p>
+            <p className="my-4">
+              What really drives me is the opportunity to solve problems for
+              users. I believe that understanding the people behind the problems
+              is key to building solutions that not only meet their needs but
+              also make their lives easier. Whether I'm collaborating with
+              product owners, designers, or users, I focus on empathy and
+              communication to create products that make a real impact.
+            </p>
+            <p className="my-4">
+              Over the years, I've had the privilege of working on a diverse
+              range of projects—from building a mobile app to help users locate
+              items, to building a data quality interface, and even streamlining
+              internationalization processes for a SAAS application. I've worked
+              in industries like sustainability and real estate, helping
+              organizations measure and manage their environmental impacts, as
+              well as building internal web and mobile apps to support retail
+              operations at a growing midwestern company.
+            </p>
+            <p className="my-4">
+              No matter the project, my goal is to bring a mix of technical
+              expertise, creativity, and empathy to the table. I’m always
+              excited to dive into new challenges and collaborate with others to
+              create something meaningful.
+            </p>
+          </article>
+        </div>
+      </AnimatedSection>
+    </section>
+  );
+};
 
 export default About;
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,5 +1,18 @@
 import Skill from "./Skill";
-import { BiLogoAngular, BiLogoAws, BiLogoCss3, BiLogoFigma, BiLogoGraphql, BiLogoHtml5, BiLogoJava, BiLogoJavascript, BiLogoReact, BiLogoSass, BiLogoTailwindCss, BiLogoTypescript } from "react-icons/bi";
+import {
+  BiLogoAngular,
+  BiLogoAws,
+  BiLogoCss3,
+  BiLogoFigma,
+  BiLogoGraphql,
+  BiLogoHtml5,
+  BiLogoJava,
+  BiLogoJavascript,
+  BiLogoReact,
+  BiLogoSass,
+  BiLogoTailwindCss,
+  BiLogoTypescript,
+} from "react-icons/bi";
 import { SiDatadog, SiJenkins, SiMixpanel, SiStorybook } from "react-icons/si";
 import { GoCopilot } from "react-icons/go";
 import { FaGears } from "react-icons/fa6";
@@ -9,103 +22,102 @@ import AnimatedSection from "./layout/AnimatedSection";
 
 const SIZE = 64;
 
-
 const skills = [
-    {
-        text: "HTML",
-        icon: <BiLogoHtml5 size={SIZE} color="#fe5326" />
-    },
-    {
-        text: "CSS",
-        icon: <BiLogoCss3 size={SIZE} color="#2d53e4" />
-    },
-    {
-        text: "JavaScript",
-        icon: <BiLogoJavascript size={SIZE} color="#f7df1d" />
-    },
-    {
-        text: "TypeScript",
-        icon: <BiLogoTypescript size={SIZE} color="#2f74c1" />
-    },
-    {
-        text: "React",
-        icon: <BiLogoReact size={SIZE} color="#4dc2d2" />
-    },
-    {
-        text: "Team Collaboration",
-        icon: <GiTeamIdea size={SIZE} color="#f6f383" />
-    },
-    {
-        text: "Infrastructure as Code",
-        icon: <FaGears size={SIZE} color="#b2cd31" />
-    },
-    {
-        text: "TailwindCSS",
-        icon: <BiLogoTailwindCss size={SIZE} color="#3e99c3" />
-    },
-    {
-        text: "SASS",
-        icon: <BiLogoSass size={SIZE} color="#d16397" />
-    },
-    {
-        text: "GraphQL",
-        icon: <BiLogoGraphql size={SIZE} color="#de33a5" />
-    },
-    {
-        text: "Java",
-        icon: <BiLogoJava size={SIZE} color="#ce2b33" />
-    },
-    {
-        text: "Figma",
-        icon: <BiLogoFigma size={SIZE} color="#29c872" />
-    },
-    {
-        text: "Mixpanel",
-        icon: <SiMixpanel size={SIZE} color="#7459f3" />
-    },
-    {
-        text: "Angular",
-        icon: <BiLogoAngular size={SIZE} color="#d6032e" />
-    },
-    {
-        text: "DataDog",
-        icon: <SiDatadog size={SIZE} color="#6729a0" />
-    },
-    {
-        text: "GitHub Copilot",
-        icon: <GoCopilot size={SIZE} color="#fff" />
-    },
-    {
-        text: "Jenkins",
-        icon: <SiJenkins size={SIZE} color="#91b9bb" />
-    },
-    {
-        text: "AWS",
-        icon: <BiLogoAws size={SIZE} color="#fa9203" />
-    },
-    {
-        text: "Storybook",
-        icon: <SiStorybook size={SIZE} color="#f74581" />
-    },
-    {
-        text: "Testing",
-        icon: <GrTest size={SIZE} color="#c597d1" />
-    },
-]
+  {
+    text: "HTML",
+    icon: <BiLogoHtml5 size={SIZE} color="#fe5326" />,
+  },
+  {
+    text: "CSS",
+    icon: <BiLogoCss3 size={SIZE} color="#2d53e4" />,
+  },
+  {
+    text: "JavaScript",
+    icon: <BiLogoJavascript size={SIZE} color="#f7df1d" />,
+  },
+  {
+    text: "TypeScript",
+    icon: <BiLogoTypescript size={SIZE} color="#2f74c1" />,
+  },
+  {
+    text: "React",
+    icon: <BiLogoReact size={SIZE} color="#4dc2d2" />,
+  },
+  {
+    text: "Team Collaboration",
+    icon: <GiTeamIdea size={SIZE} color="#f6f383" />,
+  },
+  {
+    text: "Infrastructure as Code",
+    icon: <FaGears size={SIZE} color="#b2cd31" />,
+  },
+  {
+    text: "TailwindCSS",
+    icon: <BiLogoTailwindCss size={SIZE} color="#3e99c3" />,
+  },
+  {
+    text: "SASS",
+    icon: <BiLogoSass size={SIZE} color="#d16397" />,
+  },
+  {
+    text: "GraphQL",
+    icon: <BiLogoGraphql size={SIZE} color="#de33a5" />,
+  },
+  {
+    text: "Java",
+    icon: <BiLogoJava size={SIZE} color="#ce2b33" />,
+  },
+  {
+    text: "Figma",
+    icon: <BiLogoFigma size={SIZE} color="#29c872" />,
+  },
+  {
+    text: "Mixpanel",
+    icon: <SiMixpanel size={SIZE} color="#7459f3" />,
+  },
+  {
+    text: "Angular",
+    icon: <BiLogoAngular size={SIZE} color="#d6032e" />,
+  },
+  {
+    text: "DataDog",
+    icon: <SiDatadog size={SIZE} color="#6729a0" />,
+  },
+  {
+    text: "GitHub Copilot",
+    icon: <GoCopilot size={SIZE} color="#fff" />,
+  },
+  {
+    text: "Jenkins",
+    icon: <SiJenkins size={SIZE} color="#91b9bb" />,
+  },
+  {
+    text: "AWS",
+    icon: <BiLogoAws size={SIZE} color="#fa9203" />,
+  },
+  {
+    text: "Storybook",
+    icon: <SiStorybook size={SIZE} color="#f74581" />,
+  },
+  {
+    text: "Testing",
+    icon: <GrTest size={SIZE} color="#c597d1" />,
+  },
+];
 
 export default function Skills() {
-    return (
-        <section className="h-auto xl:pt-40 pt-36 pb-5 text-text" id="skills">
-            <AnimatedSection>
-                <h3 className="text-4xl my-4 font-bold text-accent text-center">Skills</h3>
-                <div className="grid md:grid-cols-3 lg:grid-cols-5 place-items-center">
-                    {
-                        skills.map(skill => (
-                            <Skill key={skill.text} {...skill} />
-                        ))
-                    }
-                </div>
-            </AnimatedSection>
-        </section>
-    );
-};
+  return (
+    <section className="h-auto xl:pt-40 pt-36 pb-5 text-text" id="skills">
+      <AnimatedSection>
+        <h3 className="text-4xl my-4 font-bold text-accent text-center">
+          Skills
+        </h3>
+        <div className="grid md:grid-cols-3 lg:grid-cols-5 place-items-center">
+          {skills.map((skill) => (
+            <Skill key={skill.text} {...skill} />
+          ))}
+        </div>
+      </AnimatedSection>
+    </section>
+  );
+}
